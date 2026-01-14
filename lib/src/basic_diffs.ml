c0fcae322455daec64eddf351478c7829c021cf1
public-release <as-public-release@janestreet.com>
2026-01-14 02:18:38 +0000

    v0.18~preview.130.83+317

--- lib/src/basic_diffs.ml
+++ lib/src/basic_diffs.ml
@@ -40,7 +40,7 @@ module Diff_of_float = Make_atomic_with_extra_deriving (struct
        - [Float.compare Float.nan Float.nan = 0] The latter makes more sense for diffs
     *)
     let equal = [%compare.equal: t]
-    let%template[@mode local] equal = [%compare_local.equal: t]
+    let%template[@mode local] equal = ([%compare.equal: t] [@mode local])
   end)
 
 module Diff_of_int = Make_atomic_with_extra_deriving (struct
